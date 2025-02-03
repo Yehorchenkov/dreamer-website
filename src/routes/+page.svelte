ff29dd1d1ab188447b6a9ca0ee7dc6da93431931
Oleksii Yehorchenkov <alexee@ukr.net>
2025-02-03 11:58:18 +0100

    add theme, fix logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 
 <div class="mt-12">
     <div class="flex flex-col md:flex-row items-center justify-center">
-        <div class="mb-4 md:mb-0 flex-none text-center md:text-left md:w-14 md:rotate-270">
+        <div class="mb-4 md:mb-0 flex-none text-primary font-semibold text-xl text-center md:text-left md:w-14 md:rotate-270 whitespace-nowrap">
             About the project
         </div>
         <div class="overflow-visible flex-auto">
@@ -16,7 +16,7 @@
 
 <div class="mt-12">
     <div class="flex flex-col md:flex-row items-center justify-center">
-        <div class="mb-4 md:mb-0 flex-none text-center md:text-left md:w-14 md:rotate-270">
+        <div class="mb-4 md:mb-0 flex-none text-primary font-semibold text-xl text-center md:text-left md:w-14 md:rotate-270 whitespace-nowrap">
             Partners
         </div>
         <div class="overflow-visible flex-auto">
