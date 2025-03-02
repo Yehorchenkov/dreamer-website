d8ad77b29ad78ca0871c95d8b67e0b87ed0950dd
Oleksii Yehorchenkov <alexee@ukr.net>
2025-03-02 20:27:28 +0100

    add hero image

--- _site/_app/immutable/entry/start.BQKbifef.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/D-OPT5Az.js";export{t as start};
--- src/routes/main-page/hero.svelte
+++ src/routes/main-page/hero.svelte
@@ -3,13 +3,13 @@
 </script>
 
 <section class="flex items-center justify-center">
-    <div class="flex flex-col md:flex-row items-center gap-x-6 w-full m-2 md:m-10 bg-radial-[at_25%_25%] from-cyan-500 to-primary border border-gray-300 rounded-lg shadow-lg overflow-hidden">
+    <div class="flex flex-col md:flex-row items-center gap-x-6 w-full m-2 md:m-10 bg-radial-[at_25%_25%] from-yellow-700 to-yellow-400 border border-gray-300 rounded-lg shadow-lg">
         <!-- Left side: text content -->
         <div class="md:w-1/2 p-8 flex flex-col items-center justify-center text-gray-200">
             <h1 class="text-4xl font-bold mb-4">
                 DREAMER -<br /><span class="text-2xl">Transforming Education to Tackle Global Challenges</span>
             </h1>
-            <p class="text-lg mb-6 text-justify">
+            <p class="text-md lg:text-xl mb-6 text-justify">
                 The DREAMER project is poised to make a profound and lasting impact,
                 not only by enhancing project management education across Europe but also by supporting Ukraine’s journey
                 toward recovery and resilience. By fostering collaboration, promoting sustainability,
@@ -20,19 +20,13 @@
         </div>
 
         <!-- Right side: image -->
-        <div class="md:w-1/2 flex items-center justify-center p-8">
-            <div class="flex items-center justify-center w-64 h-64 animate-pulse bg-gray-300 rounded-lg md:w-96 md:h-96 dark:bg-gray-700">
-                <svg class="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                    <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
-                </svg>
-            </div>
-            <!-- <enhanced:img 
-                src="$lib/images/hero-image.webp" 
-                alt="man and woman found exit from a labyrinth" 
-                class="z-20 w-64 h-64 md:w-96 md:h-96 rounded-lg"
-            /> -->
+        <div class="md:w-1/2 flex items-center justify-center relative overflow-visible">
+            <enhanced:img 
+                src="$lib/images/hero-image.png" 
+                alt="construction site" 
+                class="z-20 h-64 w-auto md:h-full object-cover rounded-lg"
+            />
         </div>
-
     </div>
-  </section>
+</section>
 
