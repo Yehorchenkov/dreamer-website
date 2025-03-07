130c11fe5178b47ee6411706923dc492249bd3dd
Oleksii Yehorchenkov <alexee@ukr.net>
2025-03-07 08:41:15 +0100

    update linkedin reference

--- src/lib/config/site.js
+++ src/lib/config/site.js
@@ -4,7 +4,7 @@ export const siteConfig = {
     description: 'Website of the DREAMER DREAMER Project (Development and Reconstruction Empowerment through Advanced Project Management Education and Research) Erasmus+ 2024-1-SK01-KA220-HED-000248287',
     links: {
         facebook: 'https://facebook.com/groups/631008452638298',
-        linkedin: 'https://www.linkedin.com/groups/10048753',
+        linkedin: 'https://www.linkedin.com/in/dreamer-eu-044334353/',
     },
     keywords: `DREAMER, Erasmus+, Ukraine Reconstruction, Project Management, Education, Research, 2024-1-SK01-KA220-HED-000248287`,
 };
